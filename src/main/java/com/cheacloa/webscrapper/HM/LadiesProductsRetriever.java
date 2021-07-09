d70e23ca39226a798b3395850849a08d1710a3b1
Piotr Kalota <piotrkalotaspam@gmail.com>
2021-07-09 13:49:04 +0200

    Add Dresses, Shirts and Blouses and Swimwear scrapper

--- src/main/java/com/cheacloa/webscrapper/HM/LadiesProductsRetriever.java
+++ src/main/java/com/cheacloa/webscrapper/HM/LadiesProductsRetriever.java
@@ -1,8 +1,6 @@
 package com.cheacloa.webscrapper.HM;
 
-import com.cheacloa.webscrapper.HM.woman.AccessoriesScrapper;
-import com.cheacloa.webscrapper.HM.woman.DivideScrapper;
-import com.cheacloa.webscrapper.HM.woman.TrendsScrapper;
+import com.cheacloa.webscrapper.HM.woman.*;
 import com.cheacloa.webscrapper.Product;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +17,12 @@ public class LadiesProductsRetriever {
     private DivideScrapper divideScrapper;
     @Autowired
     private TrendsScrapper trendsScrapper;
+    @Autowired
+    private DressesScrapper dressesScrapper;
+    @Autowired
+    private SwimwearScrapper swimwearScrapper;
+    @Autowired
+    private ShirtsAndBlousesScrapper shirtsAndBlousesScrapper;
 
     public List<Product> run(WebDriver driver) {
         List<Product> products = new ArrayList<>();
@@ -28,6 +32,12 @@ public class LadiesProductsRetriever {
         products.addAll(divideScrapper.run(driver));
         System.out.println(products.size()); //LOG
         products.addAll(trendsScrapper.run(driver));
+        System.out.println(products.size()); //LOG
+        products.addAll(dressesScrapper.run(driver));
+        System.out.println(products.size());  //LOG
+        products.addAll(swimwearScrapper.run(driver));
+        System.out.println(products.size()); //LOG
+        products.addAll(shirtsAndBlousesScrapper.run(driver));
         System.out.println(products.size()); //LOG
 
         return products;
