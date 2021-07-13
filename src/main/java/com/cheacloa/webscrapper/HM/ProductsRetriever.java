4323c62f2f4e244e97719e6bb6b48a1ac8f90138
Piotr Kalota <piotrkalotaspam@gmail.com>
2021-07-13 22:38:55 +0200

    Add heroku deployment configuration

--- src/main/java/com/cheacloa/webscrapper/HM/ProductsRetriever.java
+++ src/main/java/com/cheacloa/webscrapper/HM/ProductsRetriever.java
@@ -6,41 +6,69 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
+@RestController
 public class ProductsRetriever {
-    @Value("${driver.path}")
+    @Value("${local.run}")
+    private boolean LOCAL_RUN;
+
+    @Value("${local.driver.path}")
     private String DRIVER_PATH;
+
+    @Value("${user.agent}")
+    private String USER_AGENT;
+
+    @Value("${google.chrome.bin}")
+    private String GOOGLE_CHROME_BIN;
+
+    @Value("${heroku.driver.path}")
+    private String CHROMEDRIVER_PATH;
+
+    @Value("${heroku.options.bin}")
+    private String HEROKU_OPTIONS_BIN;
 
     @Autowired
     private LadiesProductsRetriever ladiesProductsRetriever;
 
-    /* Only for test purposes, will be removed */
-    @PostConstruct
-    public void temporaryPostConstruct() {
-        run();
-    }
-
-    public List<Product> run() {
-        System.out.println("[INFO] Hm scrapping started"); //LOG
-
-        List<Product> products = new LinkedList<>();
-        String userAgent = "user-agent={0}Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36";
+    private HashMap<String, Object> preferencesInit() {
         HashMap<String, Object> prefs = new HashMap<>();
         // Prevent driver from loading the images - time saving
         prefs.put("profile.managed_default_content_settings.images", 2);
+        return prefs;
+    }
 
+    private WebDriver localInit() {
         System.setProperty("webdriver.chrome.driver", DRIVER_PATH);
+
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", userAgent);
-        options.setExperimentalOption("prefs", prefs);
-        WebDriver driver = new ChromeDriver(options);
+        options.addArguments("--headless", USER_AGENT);
+        options.setExperimentalOption("prefs", preferencesInit());
+        return new ChromeDriver(options);
+    }
+
+    private WebDriver herokuInit() {
+        System.setProperty("GOOGLE_CHROME_BIN", GOOGLE_CHROME_BIN);
+        System.setProperty("CHROMEDRIVER_PATH", CHROMEDRIVER_PATH);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary(HEROKU_OPTIONS_BIN);
+        options.addArguments("--headless", USER_AGENT);
+        options.setExperimentalOption("prefs", preferencesInit());
+        return new ChromeDriver(options);
+    }
+
+    @GetMapping("/hm")
+    public List<Product> run() {
+        System.out.println("[INFO] Hm scrapping started"); //LOG
+
+        WebDriver driver = LOCAL_RUN ? localInit() : herokuInit();
+        List<Product> products = new LinkedList<>();
 
         List<Product> ladies = ladiesProductsRetriever.run(driver);
 
--- src/main/java/com/cheacloa/webscrapper/WebScrapperApplication.java
+++ src/main/java/com/cheacloa/webscrapper/WebScrapperApplication.java
@@ -1,24 +1,11 @@
 package com.cheacloa.webscrapper;
 
-import com.cheacloa.webscrapper.HM.ProductsRetriever;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.context.annotation.Bean;
-
 @SpringBootApplication
 public class WebScrapperApplication {
-	@Autowired
-	private ProductsRetriever scraper;
-
-	@Bean
-	public ProductsRetriever getScraper() {
-		return new ProductsRetriever();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebScrapperApplication.class, args);
 	}
-
 }
