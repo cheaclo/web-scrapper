e5d5756ad2a6e00edcd6561f0bbd38fd6e6af86f
Piotr Kalota <piotrkalota99@gmail.com>
2021-08-05 21:19:41 +0200

    WebScrapperApplication refactor

--- src/main/java/com/cheacloa/webscrapper/WebScrapperApplication.java
+++ src/main/java/com/cheacloa/webscrapper/WebScrapperApplication.java
@@ -1,14 +1,11 @@
 package com.cheacloa.webscrapper;
 
-import com.cheacloa.webscrapper.hm.HMProductsRetriever;
-import com.cheacloa.webscrapper.reserved.ReservedProductsRetriever;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebScrapperApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(WebScrapperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebScrapperApplication.class, args);
+    }
 }
